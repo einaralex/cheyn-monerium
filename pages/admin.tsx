57de4e8e838f378608576a3ab1ffe3aed1364a11
einaralex <einaralex@gmail.com>
2022-07-23 19:24:13 +0200

    chore: add credentials and try provider

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -29,8 +29,8 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const authToken = await fetch("https://api.monerium.dev/auth/token", {
     method: "POST",
     body: new URLSearchParams({
-      client_id: "1234567890abcdef",
-      client_secret: "1234567890abcdef1234567890abcdef",
+      client_id: "8f5d738d-60c3-42f9-874b-39717ef11f5c",
+      client_secret: "69f3332b-d0f3-4048-81b9-2500eb065a2f",
       grant_type: "client_credentials",
     }),
     headers: headers,
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,49 @@ import Cookies from "cookies";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { Web3Provider } from "@ethersproject/providers";
 
 const baseUrl =
   process.env?.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://monerium-api.vercel.app"
+    ? "https://1inch-monerium.vercel.app"
     : "http://localhost:8001";
 
 const Home: NextPage<{ params: any }> = ({ params }) => {
+  const [provider, setProvider] = useState<Web3Provider>();
+  const [signer, setSigner] = useState();
   const router = useRouter();
+
+  useEffect(() => {
+    let p = new ethers.providers.Web3Provider(window.ethereum);
+    const getPermission = async () => {
+      console.log("no go?");
+      // return provider.request({ method: "eth_requestAccounts" });
+      await p.send("eth_requestAccounts", []);
+    };
+    getPermission();
+    // setProvider(p);
+  }, []);
+
+  // useEffect(() => {
+  //   if (provider) {
+  //     console.log("I NEVER GO!??");
+  //     const getPermission = async () => {
+  //       console.log("no go?");
+  //       // return provider.request({ method: "eth_requestAccounts" });
+  //     };
+  //     getPermission();
+  //     getPermission();
+  //     getPermission();
+  //     // setSigner(provider?.getSigner());
+  //     let sign = provider?.getSigner();
+  //   }
+  // }, [provider]);
 
   const emiConnect = () => {
     router.push(
-      `https://sandbox.monerium.dev/partners/piedpiper/auth?${new URLSearchParams(
+      `https://sandbox.monerium.dev/partners/1inch/auth?${new URLSearchParams(
         params
       ).toString()}`
     );
@@ -24,7 +55,7 @@ const Home: NextPage<{ params: any }> = ({ params }) => {
   return (
     <div className={styles.main}>
       <h1>Be your own bank</h1>
-
+      <p>{provider?.selectedAddress}</p>
       <button className={styles.ibanButton} onClick={() => emiConnect()}>
         <Image src="/monerium.svg" alt="me" width="24" height="24" />
         Create IBAN
@@ -51,7 +82,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const cookieName = "monerium-state";
 
   const params = {
-    client_id: "1337",
+    client_id: "7c92dcf7-b90a-4ff4-98e8-3c7d6c7f4afc",
     redirect_uri: redirectUri,
     code_challenge: codeChallenge,
     code_challenge_method: "S256",
